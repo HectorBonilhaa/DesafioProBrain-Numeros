1f238948605beaf44f9768eadfc5470227d815f9
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-12-06 10:32:34 -0300

    feat: add home page

--- DesafioProBrain/src/App.jsx
+++ DesafioProBrain/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import ModalGames from "./components/ModalGames/modalGames";
 
-import './App.css'
-
+import "./App.css";
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleGifClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-   <div>
-    <h1>Hello world</h1>
-   </div>
-  )
+    <div className="container">
+      <img
+        src="https://media.giphy.com/media/DebhXJUVWmyNDTt8Lv/giphy.gif"
+        alt="GIF"
+        className="gif"
+        onClick={handleGifClick}
+      />
+      <h1 className="main-text">Vamos Jogar um jogo? 🎮</h1>
+      {isModalOpen && <ModalGames onClose={handleCloseModal} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
