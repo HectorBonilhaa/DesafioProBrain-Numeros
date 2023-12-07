19e15b504b74f9ee6f822fb0a2bf86f8b180e3a1
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-12-07 14:11:42 -0300

    feat: add home page

---
+++ DesafioProBrain/src/pages/home/index.jsx
@@ -0,0 +1,82 @@
+import { useState, useRef, useEffect } from "react";
+import ModalGames from "../../components/ModalGames/modalGames";
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+
+import "./home.css";
+import musica from "../../assets/music/home_music.mp3";
+
+function HomePage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const audioRef = useRef(null);
+  const containerRef = useRef(null);
+
+
+  const handleGifClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleToggleMute = () => {
+    setIsMuted((prevMuted) => !prevMuted);
+    audioRef.current.muted = !audioRef.current.muted;
+  };
+
+  const handleStartAudio = () => {
+    handleToggleMute();
+    audioRef.current.volume = 0.2; // Define o volume para 50%
+    audioRef.current.play().catch((error) => {
+      console.error("Erro ao reproduzir áudio:", error);
+    });
+  };
+
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+      // Verifica se o clique foi fora do cartão
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        // Inicia a reprodução automática do áudio quando o usuário interage fora do cartão
+        handleStartAudio();
+      }
+    };
+
+    // Adiciona um ouvinte de clique ao documento
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      // Remove o ouvinte quando o componente for desmontado
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
+  return (
+    <div className="container" ref={containerRef}>
+      <p className="text-header">Clique no gif abaixo e escolha seu game!</p>
+      <img
+        src="https://media.giphy.com/media/DebhXJUVWmyNDTt8Lv/giphy.gif"
+        alt="GIF"
+        className="gif"
+        onClick={handleGifClick}
+      />
+      <div className="mid-container">
+        <h1 className="main-text">Vamos Jogar um jogo?</h1>
+
+        <audio loop ref={audioRef}>
+          <source src={musica} type="audio/mp3" />
+          Seu navegador não suporta o elemento de áudio.
+        </audio>
+        <button className="audio-btn" onClick={handleToggleMute}>
+          {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
+        </button>
+      </div>
+      {isModalOpen && <ModalGames onClose={handleCloseModal} />}
+    </div>
+  );
+}
+
+export default HomePage;
