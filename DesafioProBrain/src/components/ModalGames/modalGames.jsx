641b07f0ab05e7ef9559ff9cd1c64b9cdbef05f1
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-12-07 20:07:19 -0300

    feat: add link to all games

--- DesafioProBrain/src/components/ModalGames/modalGames.jsx
+++ DesafioProBrain/src/components/ModalGames/modalGames.jsx
@@ -1,42 +1,26 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
-import './modalGames.css';
-import EvenOrOdd from "../../pages/EvenOrOdd/evenOrOdd";
-import { Link } from 'react-router-dom';
+import "./modalGames.css";
 
+import { Link } from "react-router-dom";
 
 const ModalGames = ({ onClose }) => {
-  const [selectedGame, setSelectedGame] = useState(null);
-
-  const handleGameSelection = (game) => {
-    setSelectedGame(game);
-  };
-
-  // Adicione a navegação condicional com base no jogo selecionado
-  let gameComponent;
-  switch (selectedGame) {
-    case "parImpar":
-      gameComponent = <EvenOrOdd />;
-      break;
-    // Adicione casos para outros jogos, se necessário
-    default:
-      gameComponent = null;
-  }
-
   return (
     <div className="modal-overlay">
       <div className="modal">
         <h2>Escolha um jogo:</h2>
-        <Link to="/evenOrOdd" style={{ textDecoration: 'none' }}>
-        <button onClick={() => handleGameSelection("parImpar")}>Ímpar / Par</button>
+        <Link to="/evenOrOdd" style={{ textDecoration: "none" }}>
+          <button>Ímpar / Par</button>
         </Link>
 
-        <button onClick={() => handleGameSelection("numeroPrimo")}>Número Primo</button>
-        <button onClick={() => handleGameSelection("fibonacci")}>Sequência Fibonacci</button>
-        <button onClick={() => handleGameSelection("Múltiplos de 4 Pin")}>Múltiplos 4 Pin</button>
-
-        {/* Renderiza o componente do jogo selecionado, se houver */}
-        {gameComponent}
+        <Link to="/primeNumber" style={{ textDecoration: "none" }}>
+          <button>Número Primo</button>
+        </Link>
+        <Link to="/fibonacci" style={{ textDecoration: "none" }}>
+          <button>Sequência Fibonacci</button>
+        </Link>
+        <Link to="/fourPin" style={{ textDecoration: "none" }}>
+          <button>Múltiplos 4 Pin</button>
+        </Link>
 
         <button className="close-button" onClick={onClose}>
           Fechar
