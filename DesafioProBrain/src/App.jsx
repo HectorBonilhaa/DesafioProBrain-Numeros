e81413d4f80c03eca16bba333068fdb6e1825653
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-12-07 14:11:11 -0300

    feat: add navigate

--- DesafioProBrain/src/App.jsx
+++ DesafioProBrain/src/App.jsx
@@ -1,71 +1,15 @@
-import { useState, useRef, useEffect } from "react";
-import ModalGames from "./components/ModalGames/modalGames";
-import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
-
-import "./App.css";
-
-// Importe a música
-import musica from "./assets/music/home_music.mp3";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import EvenOrOdd from "./pages/EvenOrOdd/evenOrOdd";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const audioRef = useRef(null);
-  const [isMuted, setIsMuted] = useState(false);
-
-  const handleGifClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleToggleMute = () => {
-    setIsMuted((prevMuted) => !prevMuted);
-    audioRef.current.muted = !audioRef.current.muted;
-  };
-
-  const handleStartAudio = () => {
-    audioRef.current.play().catch((error) => {
-      console.error("Erro ao reproduzir áudio:", error);
-    });
-  };
-
-  useEffect(() => {
-    // Inicia a reprodução automática do áudio quando o componente é montado
-    handleStartAudio();
-
-    // Adiciona um ouvinte de clique no componente para iniciar a reprodução
-    document.addEventListener("click", handleStartAudio);
-
-    return () => {
-      // Remove o ouvinte quando o componente for desmontado
-      document.removeEventListener("click", handleStartAudio);
-    };
-  }, []); // O array vazio assegura que o useEffect seja executado apenas uma vez, equivalente ao componentDidMount
-
   return (
-    <div className="container">
-      <audio loop ref={audioRef}>
-        {/* Use o caminho relativo para a música */}
-        <source src={musica} type="audio/mp3" />
-        Seu navegador não suporta o elemento de áudio.
-      </audio>
-
-      <img
-        src="https://media.giphy.com/media/DebhXJUVWmyNDTt8Lv/giphy.gif"
-        alt="GIF"
-        className="gif"
-        onClick={handleGifClick}
-      />
-      <h1 className="main-text">
-        Vamos Jogar um jogo?
-        <button className="audio-btn" onClick={handleToggleMute}>
-          {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
-        </button>
-      </h1>
-      {isModalOpen && <ModalGames onClose={handleCloseModal} />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/evenOrOdd" element={<EvenOrOdd />} />
+      </Routes>
+    </Router>
   );
 }
 
