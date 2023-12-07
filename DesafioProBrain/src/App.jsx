9b1808319aa098a25277c77bfaac77fb3135c80f
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-12-07 20:04:31 -0300

    chore: add setup navigate to other pages

--- DesafioProBrain/src/App.jsx
+++ DesafioProBrain/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import EvenOrOdd from "./pages/EvenOrOdd/evenOrOdd";
+import PrimeNumber from "./pages/PrimeNumber/primeNumber";
+import Fibonacci from "./pages/FibonacciSequence/fibonacci";
+import FourPin from "./pages/FourPin/fourPin";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/evenOrOdd" element={<EvenOrOdd />} />
+        <Route path="/primeNumber" element={<PrimeNumber />} />
+        <Route path="/fibonacci" element={<Fibonacci />} />
+        <Route path="/fourPin" element={<FourPin />} />
       </Routes>
     </Router>
   );
